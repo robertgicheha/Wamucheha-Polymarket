6a3dcac96b4e8cc99b4dcb69343f13419b26f61e
Robert Gicheha <robertgicheha3@gmail.com>
2026-07-27 23:40:25 +0300

    Fix the data

--- data/__init__.py
+++ data/__init__.py
@@ -1,17 +1,12 @@
+import data.market_parser
 from data.price_feeds import DataAggregator
 from data.news_feeds import NewsFeedAggregator
 from data.market_parser import (
     parse_crypto_market,
-    parse_election_market,
-    parse_sports_market,
-    parse_macro_market,
 )
 
 __all__ = [
     "DataAggregator",
     "NewsFeedAggregator",
-    "parse_crypto_market",
-    "parse_election_market",
-    "parse_sports_market",
-    "parse_macro_market",
+    "parse_crypto_market"
 ]
